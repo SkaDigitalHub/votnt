fd04057279c19575c45ac713224da15b0b607cba
SkaDigitalHub <skadigitalhub@gmail.com>
2025-05-15 05:47:07 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'WACI-Administrative-App';
+const urlsToCache = ['/', '/index.html', '/img/logo.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
